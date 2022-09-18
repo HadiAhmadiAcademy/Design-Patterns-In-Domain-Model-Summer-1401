97d701b0d53765567f737c9d8bc7d76df239f143
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2022-09-19 03:03:01 +0430

    session 06

---
+++ Session06/BasketSample/IDiscountStrategy.cs
@@ -0,0 +1,15 @@
+﻿namespace BasketSample
+{
+    public interface IDiscountStrategy
+    {
+        long Calculate(long totalPrice);
+    }
+
+    public class VipCustomerOff : IDiscountStrategy
+    {
+        public long Calculate(long totalPrice)
+        {
+            return (long)(totalPrice  * 0.3);
+        }
+    }
+}
---
+++ Session06/LoggingSample/ILogger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LoggingSample
+{
+    public interface ILogger
+    {
+        void WriteDebug(string message);
+        void WriteInfo(string message);
+        void WriteWarning(string message);
+    }
+
+    public class Log4NetAdapter : ILogger //, Log4NetLogger
+    {
+        //... 
+        public void WriteDebug(string message)
+        {
+            //base.Debug(...)
+        }
+
+        public void WriteInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SeriLogAdapter : ILogger
+    {
+        // Serilog.ILogger _logger;
+        public void WriteDebug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
